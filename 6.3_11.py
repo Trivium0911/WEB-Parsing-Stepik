c60a0f323188231867a2d88f895649d67b063327
Trivium0911 <triv_1993@mail.ru>
2022-05-17 14:26:26 +0300

    added task 6.3_11

---
+++ 6.3_11.py
@@ -0,0 +1,23 @@
+"""
+
+Откройте сайт (http://parsinger.ru/selenium/4/4.html);
+Установите все чек боксы в положение checked при помощи selenium и метода click();
+Когда все чек боксы станут активны, нажмите на кнопку;
+Скопируйте число которое появится на странице;
+Результат появится в <p id="result">Result</p>;
+Вставьте число в поле для ответа.
+"""
+
+
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+with webdriver.Chrome() as browser:
+    browser.get('http://parsinger.ru/selenium/4/4.html')
+    for elem in browser.find_elements(By.CLASS_NAME, "check"):
+        elem.click()
+    browser.find_element(By.CLASS_NAME, "btn").click()
+    time.sleep(2)
+    print(browser.find_element(By.ID,"result").text)
